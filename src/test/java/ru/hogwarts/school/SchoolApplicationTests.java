9d2739ff083157828a434f1ddff9de511177ccc2
dimka <dimka411841994@gmail.com>
2022-10-17 14:15:50 +0300

    HomeWork тестирование Web

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,20 @@
 package ru.hogwarts.school;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.hogwarts.school.controllers.StudentController;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
+    @Autowired
+    private StudentController studentController;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        assertThat(studentController);
     }
-
 }
